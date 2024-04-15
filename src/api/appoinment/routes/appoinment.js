91cb1ac4fda5352a350d115e46f817af9e9e8a5f
Ishraq-Ul-Alam <105166186+Ishraq-Ul-Alam@users.noreply.github.com>
2024-04-15 23:54:37 +1000

    Backend

---
+++ src/api/appoinment/routes/appoinment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appoinment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::appoinment.appoinment');
