df5269792976703393388fac0c8b935c2d4fe079
Ishraq-Ul-Alam <105166186+Ishraq-Ul-Alam@users.noreply.github.com>
2024-04-16 15:22:53 +1000

    Final

---
+++ src/api/appoinment/services/appoinment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appoinment service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::appoinment.appoinment');
